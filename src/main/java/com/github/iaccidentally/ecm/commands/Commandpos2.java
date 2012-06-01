dc44dbd9d3f01d25e3aff6220fc5fa746fc2737c
Whisk <whisk@w-c.us>
2012-06-01 12:29:51 -0800

    Added Commandpos2

--- src/main/java/com/github/iaccidentally/ecm/commands/Commandpos2.java
+++ src/main/java/com/github/iaccidentally/ecm/commands/Commandpos2.java
@@ -1,14 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.github.iaccidentally.ecm.commands;
 
-/**
- *
- * @author Whisk
- */
-public class Commandpos2 {
+import com.github.iaccidentally.ecm.CommandManager;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
 
+public class Commandpos2 {
+    public static void run(Command cmd, String[] args)
+    {
+        if(!Util.isPlayer() || !Util.senderHasPermission("ecm.select"))
+        {
+            Util.sendDenied(args);
+            return;
+        }
+        
+        Location loc = null;
+        Player player = (Player) CommandManager.getSender();
+        
+        if(args[1].equalsIgnoreCase("pos2"))
+        {
+            loc = (Location) player.getLocation();
+            Util.sendSuccess ("Second point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
+            CommandManager.setLocation(loc, 1);
+        }
+        else
+        {
+            Util.sendInvalid(cmd, args);
+        }
+    }
 }
