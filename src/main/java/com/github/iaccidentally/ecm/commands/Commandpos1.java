15a5254a9b6f10e789ab8b2e25b815f96647762f
Whisk <whisk@w-c.us>
2012-06-01 12:25:58 -0800

    added the pos1 command

--- src/main/java/com/github/iaccidentally/ecm/commands/Commandpos1.java
+++ src/main/java/com/github/iaccidentally/ecm/commands/Commandpos1.java
@@ -5,11 +5,32 @@
 
 package com.github.iaccidentally.ecm.commands;
 
-/**
- *
- * @author Whisk
- */
-public class Commandpos1 {
-    
+import com.github.iaccidentally.ecm.CommandManager;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
 
+public class Commandpos1 {
+    public static void run(Command cmd, String[] args)
+    {
+        if(!Util.isPlayer() || !Util.senderHasPermission("ecm.select"))
+        {
+            Util.sendDenied(args);
+            return;
+        }
+        
+        Location loc = null;
+        Player player = (Player) CommandManager.getSender();
+        
+        if(args[1].equalsIgnoreCase("pos1"))
+        {
+            loc = (Location) player.getLocation();
+            Util.sendSuccess ("First point selected (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
+            CommandManager.setLocation(loc, 0);
+        }
+        else
+        {
+            Util.sendInvalid(cmd, args);
+        }
+    }
 }
